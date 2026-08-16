a072310f7311d61357567c7ab490252cf6e3f7cd
moyan <17349845+moyan1bz@user.noreply.gitee.com>
2026-08-16 16:56:19 +0000

    add subscribeuser.sub.js.
    Signed-off-by: moyan <17349845+moyan1bz@user.noreply.gitee.com>

---
+++ subscribeuser.sub.js
@@ -0,0 +1,8 @@
+// ==UserSubscribe==
+// @name         xxx
+// @description  订阅xxx系列脚本
+// @version      0.1.0
+// @author       You
+// @connect      www.baidu.com
+// @scriptUrl    https://gitee.com/moyan1bz/downloadconfig/raw/master/javascript.user.js
+// ==/UserSubscribe==
